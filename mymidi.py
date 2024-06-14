2494182e8c41dcc121bd728674ca3980b88a5361
Marshal Horn <kamocat@gmail.com>
2024-06-13 19:57:02 -0700

    Transcription works poorly. Midi timing also wrong

--- mymidi.py
+++ mymidi.py
@@ -1,13 +1,16 @@
 import mido
+import transcribe
 
 mid = mido.MidiFile()
 track = mido.MidiTrack()
 mid.tracks.append(track)
-track.append(mido.Message('program_change', program=8, time=0))
+track.append(mido.Message('program_change', program=0, time=0))
 
-for i in range(60,73):
-    track.append(mido.Message('note_on',note=i, time=240, velocity=127))
-    track.append(mido.Message('note_off',note=i, time=1, ))
+melody, lengths = transcribe.from_file('wav/sample8.wav')
+
+for m, l in zip(melody, lengths):
+    track.append(mido.Message('note_on',note=m, time=int(480 * l), velocity=127))
+    #track.append(mido.Message('note_off',note=m, time=1, ))
 
 
 mid.save('new_song.mid')
--- transcribe.py
+++ transcribe.py
@@ -4,67 +4,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import floor, ceil
 
-fs, a = read('wav/sample8.wav')
-print(f'File dimensions: {a.shape}')
-N = len(a)
-rHz = 1 #Frequency resolution in Hz
-rs = 0.01 #Temporal resolution in seconds
+def from_file(fname):
+    fs, a = read(fname)
+    print(f'{fname} dimensions: {a.shape}')
+    N = len(a)
+    rHz = 10 #Frequency resolution in Hz
+    rs = 0.01 #Temporal resolution in seconds
 
-# Set up the FFT
-SFT = sig.ShortTimeFFT(sig.windows.hamming(fs//rHz, sym=False),
-                            int(fs*rs), fs, scale_to='magnitude')
+    # Set up the FFT
+    SFT = sig.ShortTimeFFT(sig.windows.hamming(fs//rHz, sym=False),
+                                int(fs*rs), fs, scale_to='magnitude')
 
-# Perform FFT
-Sx = SFT.stft(a)
+    # Perform FFT
+    Sx = SFT.stft(a)
+    mag = np.abs(Sx)
 
-print(f'FFT dimensions: {Sx.shape}')
+    min_note = 0
+    max_note = 100
+    max_pitch = mag.shape[0]
+    tonotes= np.zeros((max_pitch, max_note-min_note))
+    lo = 2**(-1/24)
+    hi = 2**(1/24)
 
-mag = np.abs(Sx)
+    for i in range(min_note, max_note ):
+        # Convert MIDI note to center pitch
+        pitch = 440 / rHz * (2**((i-69)/12)) 
+        n = 0
+        for amp in [1, 0.5, 0.3, 0.25]:
+            n += pitch 
+            # Generate high and low range for each overtone
+            lo2 = int(floor(lo * n))
+            if lo2 >= max_pitch-1:
+                break #We ran out of spectrum
+            hi2 = int(min(ceil(hi * n + 1),max_pitch))
+            # Update the transform matrix
+            tonotes[lo2:hi2,i-min_note]=amp
 
-min_note = 0
-max_note = 127
-max_pitch = mag.shape[0]
-tonotes= np.zeros((max_pitch, max_note-min_note))
-lo = 2**(-1/24)
-hi = 2**(1/24)
+    envelope = np.max(mag, axis=0)
+    keys = np.argmax(np.matmul(mag.T, tonotes), axis=1)
 
-for i in range(min_note, max_note ):
-    # Convert MIDI note to center pitch
-    pitch = 440 / rHz * (2**((i-69)/12)) 
-    #print(f'key {i-min_note} pitch {pitch:.0f} Hz')
-    n = 0
-    for amp in [1,.3,.1,.03]:
-        n += pitch 
-        # Generate high and low range for each overtone
-        lo2 = int(floor(lo * n))
-        if lo2 >= max_pitch-1:
-            #print(f'Note {i} pitch {pitch*rHz} too high at amp {amp}')
-            break #We ran out of spectrum
-        hi2 = int(min(ceil(hi * n + 1),max_pitch))
-        # Update the transform matrix
-        tonotes[lo2:hi2,i-min_note]=amp
-print(f'Transform weights: {np.sum(tonotes,axis=0)}')
-
-
+    # Get note start times
+    attack = sig.savgol_filter(envelope, 10, 2, deriv=1, mode='nearest')
+    thresh = np.max(attack) * 0.1
+    times = sig.find_peaks(attack, height=thresh)[0]
+    melody = keys[times]
     
-notes = np.matmul(mag.T, tonotes)
-print(f'Notes matrix shape: {notes.shape}')
-
-#plt.imshow(notes.T, cmap='gray', vmin='0', vmax=notes.max())
-#plt.show()
-
-if 1:
-    fig1, ax1 = plt.subplots()
-    t_lo,  t_hi = SFT.extent(N)[:2]
-    ax1.set_title("Note intensity plot")
-    ax1.set(xlabel=f"Time $t$ in seconds ({SFT.p_num(N)} slices, " +
-                   rf"$\Delta t = {SFT.delta_t:g}\,$s)",
-            ylabel=f"Midi note",
-            xlim=(t_lo, t_hi))
-
-    im1 = ax1.imshow(notes.T, origin='lower', aspect='auto',
-                     extent=SFT.extent(N), cmap='viridis')
-    fig1.colorbar(im1, label="Magnitude $|S_x(t, f)|$")
-
-    fig1.tight_layout()
+    '''
+    # Plot the data
+    fig, [[ax1,ax2],[ax3,ax4]]= plt.subplots(2,2)
+    ax4.plot(attack, color='tab:green')
+    ax3.step(times, melody)
+    color = 'tab:blue'
+    ax2.set_ylabel('Amplitude')
+    ax2.plot(envelope, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    color = 'tab:red'
+    ax1.set_xlabel(f'Time ({rs} seconds)')
+    ax1.set_ylabel('MIDI key')
+    ax1.plot(keys, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    fig.tight_layout()
     plt.show()
+    '''
+    times = np.append(times, len(attack))
+    durations = rs * (times[1:] - times[:-1])
+
+    return melody, durations
+
+if __name__ == '__main__':
+    from_file('wav/sample8.wav')
+
