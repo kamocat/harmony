625670fcf5072ef4f553dcb122c6b3035246a090
Marshal Horn <kamocat@gmail.com>
2024-12-30 11:46:12 -0800

    piptrack does not seem to work

--- spectrum.py
+++ spectrum.py
@@ -14,11 +14,15 @@ def spectrum(infile, outfile=None):
     D = librosa.stft(a, n_fft=fs//rHz, hop_length=int(fs*rs) )
     S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
 
-    fig, (ax1,ax2) = plt.subplots()
+    fig, (ax1,ax2) = plt.subplots(2)
     ax1.set_title("Spectral intensity plot (librosa)")
     ax1.set(xlabel=f"Time (seconds * {rs})")
     img = librosa.display.specshow(S_db, ax=ax1)
     fig.colorbar(img, ax=ax1)
+
+    ax2.set_title("Estimated pitch")
+    pitches, magnitudes = librosa.piptrack(S=D)
+    ax2.plot(pitches)
 	
     if outfile is None:
         plt.show()
